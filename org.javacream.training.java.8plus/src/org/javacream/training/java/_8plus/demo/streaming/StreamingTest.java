2801501fe87a392bf6616235b35c90ee8105585c
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-11-20 09:27:02 +0100

    add forEach

---
+++ org.javacream.training.java.8plus/src/org/javacream/training/java/_8plus/demo/streaming/StreamingTest.java
@@ -0,0 +1,50 @@
+package org.javacream.training.java._8plus.demo.streaming;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+public class StreamingTest {
+
+	@Test public void testList() {
+		List<String> names = List.of("Hugo", "Emil", "Fritz");
+		classicForLoop(names);
+		forEachForLoop(names);
+		modern(names);
+		
+	}
+
+	private void modern(Collection<String> names) {
+		System.out.println("MODERN");
+//		names.forEach(System.out::println);
+//		names.forEach(element -> System.out.println(element));
+		Stream<String> namesStream = names.stream();
+		namesStream.forEach(System.out::println);
+		names.stream().forEach(System.out::println);
+	}
+
+	private void forEachForLoop(Collection<String> names) {
+		System.out.println("FOR EACH LOOP");
+		//Lies als: "for each name in names"
+		for (String name : names) {
+			System.out.println(name);
+		}
+	}
+
+	@Test public void testSet() {
+		Set<String> names = Set.of("Hugo", "Emil", "Fritz");
+		//classicForLoop(names);
+		forEachForLoop(names);
+	}
+
+	private void classicForLoop(List<String> list) {
+		System.out.println("CLASSICAL");
+		int listSize = list.size();
+		for (int i = 0; i < listSize; i++) {
+			System.out.println(list.get(i));
+		}
+	}
+}
