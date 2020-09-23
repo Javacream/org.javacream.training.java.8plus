c2381ee7361ada951bdedb0b2061b67efb06ce0a
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-09-23 15:50:05 +0200

    solution streaming

--- org.javcream.training.java8plus/src/test/java/org/javcream/training/java8plus/demo/collections/CollectionsDemo.java
+++ org.javcream.training.java8plus/src/test/java/org/javcream/training/java8plus/demo/collections/CollectionsDemo.java
@@ -1,21 +1,28 @@
 package org.javcream.training.java8plus.demo.collections;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CollectionsDemo {
-
-	@Test
-	public void testList() {
-		ArrayList<String> names = new ArrayList();
+	private List<String> names;
+	
+	@Before public void init() {
+		names = new ArrayList<>();
 		names.add("Hugo");
 		names.add("Emil");
 		names.add("Fritz");
 		names.add("Eduard");
+
+	}
+	//@Test
+	public void testList() {
 
 		Stream<String> namesStream = names.stream();
 		Stream<String> filteredStream = namesStream.filter(name -> name.startsWith("E"));
@@ -25,17 +32,23 @@ public class CollectionsDemo {
 		System.out.println(resultList);
 	}
 
-	@Test
+	//@Test
 	public void testListCompact() {
 
-		ArrayList<String> names = new ArrayList<>();
-		names.add("Hugo");
-		names.add("Emil");
-		names.add("Fritz");
-		names.add("Eduard");
 		System.out.println(names.stream().filter(name -> name.startsWith("E")).map(name -> name.length())
 				.collect(Collectors.toList()));
 
 	}
+	
+	@Test public void testCollection() {
+		System.out.println(handleStream(names.stream()));
+	}
+	@Test public void testFile() throws Exception{
+		System.out.println(handleStream(Files.lines(Paths.get("names.txt"))));
+	}
 
+	private List<String> handleStream(Stream<String> data) {
+		return data.filter(name -> name.startsWith("E")).filter(name -> name.length() > 4)
+		.collect(Collectors.toList());
+	}
 }
