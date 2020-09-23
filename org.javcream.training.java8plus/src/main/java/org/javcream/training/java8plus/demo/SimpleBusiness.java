0887e82549fa8b0765a66e5b89a308266277ca52
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-09-23 10:21:05 +0200

    add simple java

---
+++ org.javcream.training.java8plus/src/main/java/org/javcream/training/java8plus/demo/SimpleBusiness.java
@@ -0,0 +1,8 @@
+package org.javcream.training.java8plus.demo;
+
+public class SimpleBusiness {
+	public String greet(String message) {
+		return message;
+	}
+
+}
---
+++ org.javcream.training.java8plus/src/test/java/org/javcream/training/java8plus/demo/SimpleBusinessTest.java
@@ -0,0 +1,10 @@
+package org.javcream.training.java8plus.demo;
+
+import org.junit.Test;
+
+public class SimpleBusinessTest {
+	@Test
+	public void testIt() {
+
+	}
+}
